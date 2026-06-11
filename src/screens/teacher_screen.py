19a685972b74534f5f01ffeb0ef245e1f1eb9e8e
DhiaShams <dhiadhilu123@gmail.com>
2026-06-11 19:36:46 +0530

    teacher auth db setup

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -2,6 +2,21 @@ import streamlit as st
 
 from src.ui.base_layout import style_bg_dashboard,style_base_layout
 from src.components.header import header_dashboard
+from src.database.db import check_teacher_exist,create_teacher,teacher_login
+
+def register_teacher(tr_username,tr_name,tr_pwd,tr_confirm_pwd):
+    if not tr_username or not tr_name or not tr_pwd:
+        return False,"All fields are required!"
+    if check_teacher_exist(tr_username):
+        return False,"Username already taken!"
+    if tr_pwd!=tr_confirm_pwd:
+        return False,"Passwords doesn't match!"
+    
+    try:
+        create_teacher(tr_username,tr_pwd,tr_name)
+        return True,"Successfully Created User! Login Now"
+    except Exception as e:
+        return False,"Unexpected error!"
 
 def teacher_screen():
     style_bg_dashboard()
@@ -35,7 +50,14 @@ def teacher_screen_login():
     btn1,btn2=st.columns(2)
 
     with btn1:
-        st.button('Login Now',icon=':material/passkey:',shortcut='control+enter',width='stretch')
+        if st.button('Login Now',icon=':material/passkey:',shortcut='control+enter',width='stretch'):
+            if teacher_login(tr_username,tr_pwd):
+                st.toast("Welcome back!",icon="👋")
+                import time
+                time.sleep(2)
+                st.rerun()
+            else:
+                st.error("Invalid credentials")
 
     with btn2:
         if st.button('Register Instead',icon=':material/passkey:',width='stretch',type='primary'):
@@ -48,10 +70,10 @@ def teacher_screen_register():
     st.space()
     st.space()
 
-    st_username=st.text_input("Enter username",placeholder="@aneeta")
-    st_name=st.text_input("Enter name",placeholder="Aneeta Rose")
-    st_pwd=st.text_input("Enter password",type="password",placeholder="Enter password")
-    st_confirm_pwd=st.text_input("Confirm password",type="password",placeholder="Confirm your password")
+    tr_username=st.text_input("Enter username",placeholder="@aneeta")
+    tr_name=st.text_input("Enter name",placeholder="Aneeta Rose")
+    tr_pwd=st.text_input("Enter password",type="password",placeholder="Enter password")
+    tr_confirm_pwd=st.text_input("Confirm password",type="password",placeholder="Confirm your password")
 
     st.divider()
 
@@ -59,7 +81,16 @@ def teacher_screen_register():
 
     
     with btn1:
-        st.button('Register Now',icon=':material/passkey:',width='stretch',type='primary',shortcut='control+enter')
+        if st.button('Register Now',icon=':material/passkey:',width='stretch',type='primary',shortcut='control+enter'):
+            success,msg=register_teacher(tr_username,tr_name,tr_pwd,tr_confirm_pwd)
+            if success:
+                st.success(msg)
+                import time
+                time.sleep(2)
+                st.session_state.teacher_login_type="login"
+                st.rerun()
+            else:
+                st.error(msg)
 
     with btn2:
         if st.button('Login Instead',icon=':material/passkey:',width='stretch'):
